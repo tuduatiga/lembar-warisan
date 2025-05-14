6751beec748a83490c7ad55a3c912c5ca5b178c2
Ilham <jangantanyaaku14@gmail.com>
2025-05-14 09:10:24 +0800

    feat: adjustable generation depth

--- scripts/gen_marker.gd
+++ scripts/gen_marker.gd
@@ -2,10 +2,22 @@ class_name GenMarker
 extends Node2D
 
 @export var weighted_scenes: Array[WeightedScene]
+@export var depth: int = 0
+@export_file("*.tscn") var fallback_scene: String
 
 
 func _ready() -> void:
 	if self.weighted_scenes.size() <= 0:
+		return
+
+	if self.depth <= 0:
+		if not self.fallback_scene:
+			return
+
+		var FallbackScene = load(self.fallback_scene)
+		if FallbackScene:
+			self.get_child(0).add_child.call_deferred(FallbackScene.instantiate())
+
 		return
 
 	var cumulative_weights: Array = self.weighted_scenes.reduce(
@@ -25,4 +37,10 @@ func _ready() -> void:
 	)
 
 	if chosen_scene:
-		self.get_child(0).add_child.call_deferred(chosen_scene.instantiate())
+		var chosen_scene_instance: Node2D = chosen_scene.instantiate()
+
+		for marker in chosen_scene_instance.find_children("*", "GenMarker"):
+			marker.depth = max(self.depth - 1, 0)
+			pass
+		
+		self.get_child(0).add_child.call_deferred(chosen_scene_instance)
