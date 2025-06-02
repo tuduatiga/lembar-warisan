72f20cdbc0f8e60a2b13087c900d71d2f6b08912
Pahril <88573655+visoredkon@users.noreply.github.com>
2025-06-03 02:30:00 +0800

    feat: simple iframe

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -5,20 +5,32 @@ signal damage_taken(health: int)
 @export var max_health: int
 
 var health: int
+var is_invincible: bool = false
+
 var _health_bar: HealthBar
+var _iframe_delay: float
 
 
 func _ready() -> void:
 	self.health = self.max_health
 	self._health_bar = self.owner.find_child("HealthBar")
+	self._iframe_delay = 1.0 if self.owner.get_groups().has("Player") else 0.5
 
 	if self._health_bar:
 		self._health_bar.init(self.health)
 
 
+func _iframe() -> void:
+	self.is_invincible = true
+	await get_tree().create_timer(self._iframe_delay).timeout
+	self.is_invincible = false
+
+
 func damage(attack: Attack) -> void:
-	if self.health <= 0:
+	if self.is_invincible or self.health <= 0:
 		return
+
+	self._iframe()
 
 	self.health -= attack.damage
 
