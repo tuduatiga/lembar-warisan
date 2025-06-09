7d0d289e218d56c7cf7847e0dd604bd200f2426c
Pahril <88573655+visoredkon@users.noreply.github.com>
2025-06-10 03:34:27 +0800

    fix: settings menu tabs

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -3,9 +3,12 @@ extends Control
 @onready var _margin_container: MarginContainer = $MarginContainer
 @onready var _settings_menu: SettingsMenu = $SettingsMenu
 
-@onready var _play_button: Button = $MarginContainer/GridContainer/VBoxContainer/VBoxContainer/PlayButton
-@onready var _settings_button: Button = $MarginContainer/GridContainer/VBoxContainer/VBoxContainer/SettingsButton
-@onready var _exit_button: Button = $MarginContainer/GridContainer/VBoxContainer/VBoxContainer/ExitButton
+@onready
+var _play_button: Button = $MarginContainer/GridContainer/VBoxContainer/VBoxContainer/PlayButton
+@onready
+var _settings_button: Button = $MarginContainer/GridContainer/VBoxContainer/VBoxContainer/SettingsButton
+@onready
+var _exit_button: Button = $MarginContainer/GridContainer/VBoxContainer/VBoxContainer/ExitButton
 
 @onready var _game: PackedScene = preload("res://scenes/game.tscn")
 
@@ -30,7 +33,6 @@ func _on_settings_button_pressed() -> void:
 
 func _on_exit_settings_menu() -> void:
 	self._margin_container.show()
-	self._settings_menu.hide()
 
 
 func _on_exit_button_pressed() -> void:
--- scripts/menu/settings/settings_menu.gd
+++ scripts/menu/settings/settings_menu.gd
@@ -2,7 +2,7 @@ class_name SettingsMenu extends Control
 
 signal exit_settings_menu
 
-@onready var _exit_button: Button = $MarginContainer/HBoxContainer/ExitButton
+@onready var _exit_button: Button = $MarginContainer/VBoxContainer/ExitButton
 
 
 func _ready() -> void:
@@ -14,3 +14,5 @@ func _ready() -> void:
 
 func _on_exit_button_pressed() -> void:
 	self.exit_settings_menu.emit()
+	self.hide()
+	self.set_process(false)
